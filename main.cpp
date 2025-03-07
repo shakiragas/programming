5bb9363ec8a2560f5b2a435f96e546b0984c44e7
shakiragas <149084984+shakiragas@users.noreply.github.com>
2025-03-07 07:14:46 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Element {
+    string name;
+    int intValue;
+    double doubleValue;
+    Element* next;
+};
+
+class List {
+public:
+    Element* head = nullptr;
+
+    List(){}
+
+    void add(const string& name, int intValue, double doubleValue) {
+        Element* newElement = new Element{name, intValue, doubleValue};
+        if (!head) {
+            head = newElement;
+        } else {
+            Element* temp = head;
+            while (temp->next) {
+                temp = temp->next;
+            }
+            temp->next = newElement;
+        }
+    }
+
+    void display() const {
+        Element* temp = head;
+        while (temp) {
+            cout << "Name: " << temp->name
+                 << ", Int Value: " << temp->intValue
+                 << ", Double Value: " << temp->doubleValue << endl;
+            temp = temp->next;
+        }
+    }
+
+    ~List() {
+        while (head) {
+            Element* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+};
+
+int main() {
+    List myList;
+    string command;
+
+    while (true) {
+        cout << "Введите команду (add, display, exit): ";
+        cin >> command;
+
+        if (command == "add") {
+            string name;
+            int intValue;
+            double doubleValue;
+            cout << "Введите имя, целое и дробное значение: ";
+            cin >> name >> intValue >> doubleValue;
+            myList.add(name, intValue, doubleValue);
+        } else if (command == "display") {
+            myList.display();
+        } else if (command == "exit") {
+            break;
+        } else {
+            cout << "Неизвестная команда. Попробуйте снова." << endl;
+        }
+    }
+
+    return 0;
+}
