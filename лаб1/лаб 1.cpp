f2c8ce1024ec0b7fbf2450a6792b461a5c0f3bb0
shakiragas <149084984+shakiragas@users.noreply.github.com>
2025-03-21 10:33:31 +0300

    Update лаб 1.cpp

--- лаб1/лаб 1.cpp
+++ лаб1/лаб 1.cpp
@@ -1,73 +1,158 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
-struct Element {
+struct Node {
     string name;
-    int intValue;
+    int amount;
     double doubleValue;
-    Element* next;
+    Node* next;
 };
 
-class List {
-public:
-    Element* head;
+Node* CreateNode(string newName, int n, double p) {
+    Node* newNode = new Node;
+    newNode->name = newName;
+    newNode->amount = n;
+    newNode->doubleValue = p;
+    newNode->next = NULL;
+    return newNode;
+}
 
-    List() : head(nullptr) {}
+void AddFirst(Node*& head, string newName, int n, double p) {
+    Node* newNode = CreateNode(newName, n, p);
+    newNode->next = head;
+    head = newNode;
+}
 
-    void add(const string& name, int intValue, double doubleValue) {
-        Element* newElement = new Element{name, intValue, doubleValue};
-        if (!head) {
-            head = newElement;
-        } else {
-            Element* temp = head;
-            while (temp->next) {
-                temp = temp->next;
-            }
-            temp->next = newElement;
-        }
+void AddLast(Node*& head, string newName, int n, double p) {
+    Node* newNode = CreateNode(newName, n, p);
+    if (head == NULL) {
+        head = newNode;
+        return;
     }
-
-    void display() const {
-        Element* temp = head;
-        while (temp) {
-            cout << "Name: " << temp->name
-                 << ", Int Value: " << temp->intValue
-                 << ", Double Value: " << temp->doubleValue << endl;
-            temp = temp->next;
-        }
+    Node* tmp = head;
+    while (tmp->next != NULL) {
+        tmp = tmp->next;
     }
+    tmp->next = newNode;
+}
 
-    ~List() {
-        while (head) {
-            Element* temp = head;
-            head = head->next;
-            delete temp;
-        }
+int AddAfter(Node* head, string newName, int n, double p, string nameAfter) {
+    if (head == NULL) return 1;
+    Node* tmp = head;
+    while (tmp != NULL && tmp->name != nameAfter) {
+        tmp = tmp->next;
     }
-};
+    if (tmp == NULL) return 1;
+    Node* newNode = CreateNode(newName, n, p);
+    newNode->next = tmp->next;
+    tmp->next = newNode;
+    return 0;
+}
+
+int AddBefore(Node*& head, string newName, int n, double p, string nameBefore) {
+    if (head == NULL) return 1;
+    if (head->name == nameBefore) {
+        AddFirst(head, newName, n, p);
+        return 0;
+    }
+    Node* prev = head;
+    Node* tmp = head->next;
+    while (tmp != NULL && tmp->name != nameBefore) {
+        prev = tmp;
+        tmp = tmp->next;
+    }
+    if (tmp == NULL) return 1;
+    Node* newNode = CreateNode(newName, n, p);
+    newNode->next = tmp;
+    prev->next = newNode;
+    return 0;
+}
+
+int DelNode(Node*& head, string delName) {
+    if (head == NULL) return 1;
+    if (head->name == delName) {
+        Node* tmp = head;
+        head = head->next;
+        delete tmp;
+        return 0;
+    }
+    Node* prev = head;
+    Node* tmp = head->next;
+    while (tmp != NULL && tmp->name != delName) {
+        prev = tmp;
+        tmp = tmp->next;
+    }
+    if (tmp == NULL) return 1;
+    prev->next = tmp->next;
+    delete tmp;
+    return 0;
+}
+
+void PrintList(Node* head) {
+    if (head == NULL) {
+        cout << "The list is empty" << endl;
+        return;
+    }
+    Node* tmp = head;
+    while (tmp != NULL) {
+        cout << "Product: " << tmp->name << ", Amount: " << tmp->amount << ", Price: " << tmp->doubleValue << endl;
+        tmp = tmp->next;
+    }
+}
 
 int main() {
-    List myList;
-    string command;
+    Node* head = NULL;
+    int choice, n;
+    double p;
+    string name, nameAfter, nameBefore;
 
     while (true) {
-        cout << "Введите команду (add, display, exit): ";
-        cin >> command;
+        cout << "1. Add to beginning" << endl;
+        cout << "2. Add to end" << endl;
+        cout << "3. Add after" << endl;
+        cout << "4. Add before" << endl;
+        cout << "5. Delete" << endl;
+        cout << "6. Print list" << endl;
+        cout << "7. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
 
-        if (command == "add") {
-            string name;
-            int intValue;
-            double doubleValue;
-            cout << "Введите имя, целое и дробное значение: ";
-            cin >> name >> intValue >> doubleValue;
-            myList.add(name, intValue, doubleValue);
-        } else if (command == "display") {
-            myList.display();
-        } else if (command == "exit") {
+        switch (choice) {
+        case 1:
+            cout << "Enter name, amount, doubleValue: ";
+            cin >> name >> n >> p;
+            AddFirst(head, name, n, p);
             break;
-        } else {
-            cout << "Неизвестная команда. Выберите одну из предложенных." << endl;
+        case 2:
+            cout << "Enter name, amount, doubleValue: ";
+            cin >> name >> n >> p;
+            AddLast(head, name, n, p);
+            break;
+        case 3:
+            cout << "Enter new name, amount, doubleValue, and name to add after: ";
+            cin >> name >> n >> p >> nameAfter;
+            AddAfter(head, name, n, p, nameAfter);
+            break;
+        case 4:
+            cout << "Enter new name, amount, doubleValue, and name to add before: ";
+            cin >> name >> n >> p >> nameBefore;
+            AddBefore(head, name, n, p, nameBefore);
+            break;
+        case 5:
+            cout << "Enter name to delete: ";
+            cin >> name;
+            DelNode(head, name);
+            break;
+        case 6:
+            PrintList(head);
+            break;
+        case 7:
+            return 0;
+        default:
+            cout << "Invalid choice. Try again." << endl;
+            break;
         }
     }
 
